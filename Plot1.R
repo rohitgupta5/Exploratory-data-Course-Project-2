37387a738d96c8a4f68131156ee58ba0f9b8e6c2
rohitgupta5 <rohitgupta.itbhu@gmail.com>
2017-04-26 01:07:51 +0000

    Assignment 2
    Sata Exploratory Assignment 2

---
+++ Plot1.R
@@ -0,0 +1,14 @@
+## Have total emissions from PM2.5 decreased in the United States from 1999 to 2008?
+if(!exists("NEI")){
+  NEI <- readRDS("./summarySCC_PM25.rds")
+}
+if(!exists("SCC")){
+  SCC <- readRDS("./Source_Classification_Code.rds")
+}
+# Aggregated Total By Year
+
+aggregatedTotalByYear <- aggregate(Emissions ~ year, NEI, sum)
+
+png('plot1.png')
+barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' emissions at various years'))
+dev.off()
